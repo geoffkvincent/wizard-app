ad17c0e4e149b9607cd16327afb8fd55ed3c98cb
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-02 10:41:28 -0600

    profile component

---
+++ config/initializers/devise.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Devise.setup do |config|
+  config.secret_key = 'f332de41d0abd55f3023e983ea6536040e707b589cc908ef60f4f17495ac2a717f885ebe96086e8f0da229387c641608e8414395f2b8c71d478a9a6d3da419fa'
+end

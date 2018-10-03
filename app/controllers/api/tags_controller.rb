561019cd4ab027c5b717ff2550500422d740d76a
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-03 11:36:01 -0600

    reducers add get delete

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,4 +1,9 @@
 class Api::TagsController < ApplicationController
+
+  def index
+    render json: current_user.tags
+  end
+
   def create
     tag = Tag.find_by(name: params[:tag][:name])
     if !current_user.tags.find_by(id: tag_id)
@@ -6,4 +11,10 @@ class Api::TagsController < ApplicationController
       render json: tag
     end
   end
+
+  def destroy
+    Tagging.find_by(user_id: current_user.id, tag_id: params[:id])
+    tag.destroy
+  end
+
 end
--- client/src/reducers/tags.js
+++ client/src/reducers/tags.js
@@ -1,22 +1,42 @@
 import axios from 'axios'
 
-const ADD_TAG ='ADD_TAG'
+const ADD_TAG = 'ADD_TAG'
+const TAGS = 'TAGS'
+const DELETE_TAG = 'DELETE_TAG'
+
+export const deleteTag = (id) => {
+  return (dispatch) => {
+    axios.delete(`/api/tags/${id}`)
+      .then( res => dispatch({ type: DELETE_TAG, id }) )
+  }
+}
+
+export const getTags = () => {
+  return (dispatch) => {
+    axios.get('/api/tags')
+      .then( res => dispatch({ type: TAGS, tags: res.data }) )
+  }
+}
 
 export const addTag = (tag) => {
   return (dispatch) => {
     axios.post('/api/tags', { tag })
-      .then ( res => {
+      .then( res => {
         if (res.data)
-        dispatch ({ type: ADD_TAG, tag: res.data })
+          dispatch({ type: ADD_TAG, tag: res.data })
       })
-    
   }
 }
 
 export default ( state = [], action ) => {
   switch(action.type) {
     case ADD_TAG:
-      return[...state, action.tag]
+      return [...state, action.tag]
+    case TAGS:
+      return action.tags
+    case DELETE_TAG:
+      // { type: DELETE_TAG, id: 7 }
+      return state.filter( t => t.id !== action.id )
     default:
       return state
   }
